6da55e5077a4324e66f2b4516efa074fe15175d4
icey-netizen <viny4457@gmail.com>
2026-02-20 12:49:51 +0300

    Auto-commit: 2026-02-20 12:49:51

--- healthapp/models.py
+++ healthapp/models.py
@@ -3,11 +3,6 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     fullname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15)
-    date_of_birth = models.DateField()
-    medical_history = models.TextField(blank=True)
-    def __str__(self):
-        return self.fullname
-
+    email = models.EmailField()
+  
   
